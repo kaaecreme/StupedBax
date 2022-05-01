12fb6738a0bb25cfdfdc77002f14defcb8a74a21
kaaecreme <92531107+kaaecreme@users.noreply.github.com>
2022-05-01 11:03:46 +0200

    Added more modes
    Renamed variables for better understanding.
    Added adjusting of offset for both arm and lid servo

--- StupedBaxV3/StupedBaxV3.ino
+++ StupedBaxV3/StupedBaxV3.ino
@@ -3,40 +3,46 @@
 
 volatile const int wakePin = 2;                 // pin used for waking up
 volatile const int LEDPin = 7;
-volatile const int servoPin = 9;
-volatile const int servoLidPin = 10;
+volatile const int armPin = 9;
+volatile const int lidPin = 10;
 volatile const int mosfetPin = 8;
 
 volatile int isActive;
 volatile int pos = 0;
 volatile int lidPos = 110;
+volatile int LA = 0;
+volatile int AA = 0;
 
 //Arm
 // Hit = 145
 // Close = 0
 
 //Lid
-// Open = 0 / 10
+// Open = 0 / 10 (fully) (30 - exact)
 // Closed = approx 110
  
 //Create servo
-Servo myservo;
-Servo myLidservo;
+Servo armServo;
+Servo lidServo;
 
 ///////////////////////////////////////////////////////////////////////////////////
 void setup() {
-  // put your setup code here, to run once:
+
+  ///// Settings //////
+  LA = 0; // Lid offset
+  AA = -9; // Arm offset
+  
   pinMode(wakePin, INPUT_PULLUP);
   pinMode(LEDPin, OUTPUT);
   pinMode(mosfetPin, OUTPUT);
 
   /////////////////////////////////
   digitalWrite(mosfetPin, LOW);
-  myservo.attach(servoPin);
-  myservo.write(0);
+  armServo.attach(armPin);
+  armServo.write(0);
 
-  myLidservo.attach(servoLidPin);
-  myLidservo.write(lidPos);
+  lidServo.attach(lidPin);
+  lidServo.write(lidPos);
   delay(1000);
   digitalWrite(mosfetPin, HIGH);
   /////////////////////////////////
@@ -58,7 +64,7 @@ void loop() {
     digitalWrite(LEDPin, HIGH);
     digitalWrite(mosfetPin, LOW);
  
-    switch (random(1, 4+1)) {
+    switch (random(1, 6+1)) {
 
       case 1:
         run1();
@@ -74,6 +80,14 @@ void loop() {
 
      case 4:
         run4();
+        break;
+        
+     case 5:
+        run5();
+        break;
+        
+     case 6:
+        run6();
         break;
 
       default:
@@ -124,11 +138,11 @@ void wakeUpNow() // Here goes the interrupt when getting awake
 ///////////////////////////////////////////////////////////////////////////////////
 // Tease
 void run1() {
-  openLid(10, 30);
+  openLid(30, 30);
   forward(134, 30);
   hold(2000);
   back(50, 30);
-  forward(145, 0);
+  forward(155, 0);
 
   while (isActive) {}
   backEnd(0, 20);
@@ -137,8 +151,8 @@ void run1() {
  
 //Quick
 void run2() {
-  openLid(10, 10);
-  forward(145, 1);
+  openLid(30, 10);
+  forward(155, 1);
 
   while (isActive) {}
   backEnd(0, 10); 
@@ -147,38 +161,58 @@ void run2() {
 
 //Really quick
 void run3() { 
-  openLid(10, 5);
-  forward(145, 0);
+  openLid(30, 5);
+  forward(155, 0);
 
   while (isActive) {}
   backEnd(0, 10);  
   closeLid(110, 1);
 }
 
-//Funny
-void run4() { 
-  openLid(10, 5);
+//Really really quick
+void run4() {  
+  forward(155, 0);
+
+  while (isActive) {}
+  backEnd(0, 0);   
+}
+
+//Funny open/close
+void run5() { 
+  openLid(55, 5);
   closeLid(110,5);
-  openLid(10, 5);
+  openLid(55, 5);
   closeLid(110,5);
-  openLid(10, 5);
+  openLid(55, 5);
   closeLid(110,5); 
   hold(3000);
 
-  openLid(10, 5);
-  forward(145, 1);
+  openLid(30, 5);
+  forward(155, 1);
   
   while (isActive) {}
   backEnd(0, 10);  
   closeLid(110, 10);
 }
 
+//Open lid, look
+void run6() { 
+  openLid(80, 2);
+  hold(3000);
+  closeLid(110,2);
+  hold(2000);
+  forward(155, 0);
+
+  while (isActive) {}
+  backEnd(0, 0);   
+}
 
 
 void openLid(int newPos, int speed) {
+
   for (unsigned int i = lidPos; i > newPos && isActive == 1; i -= 2) {
     lidPos -= 2;
-    myLidservo.write(i);
+    lidServo.write(i+LA);
     delay(speed);
   }
 
@@ -187,7 +221,7 @@ void openLid(int newPos, int speed) {
 void closeLid(int newPos, int speed) {
   for (unsigned int i = lidPos; i < newPos; i += 2) {
     lidPos += 2;
-    myLidservo.write(i);
+    lidServo.write(i+LA);
     delay(speed);
   }
 }
@@ -197,7 +231,7 @@ void forward(int newPos, int speed) {
 
   for (unsigned int i = pos; i < newPos && isActive == 1; i += 2) {
     pos += 2;
-    myservo.write(i);
+    armServo.write(i+AA);
     delay(speed);
   }
 }
@@ -205,7 +239,7 @@ void forward(int newPos, int speed) {
 void back(int newPos, int delayTime) {
   for (unsigned int i = pos; i > newPos && isActive == 1; i -= 2) {
     pos -= 2;
-    myservo.write(pos);
+    armServo.write(pos+AA);
     delay(delayTime);
   }
 
@@ -214,7 +248,7 @@ void back(int newPos, int delayTime) {
 void backEnd(int newPos, int delayTime) {
   for (unsigned int i = pos; i > newPos; i -= 2) {
     pos -= 2;
-    myservo.write(pos);
+    armServo.write(pos+AA);
     delay(delayTime);
   }
   pos = newPos;
